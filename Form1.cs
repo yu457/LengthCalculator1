a21d732615f03681e3884645196e04e2d97113c6
yu457 <113153008@gas.fgu.edu.tw>
2025-03-17 15:04:21 +0800

    將公尺的功能完成
    將公尺轉換其他單位的功能完成
    公分：douM * 100
    公里：douM * 0.001
    英吋：douM * 39.3701
    英呎：douM * 3.28084
    碼：douM * 1.09361

--- Form1.cs
+++ Form1.cs
@@ -27,15 +27,21 @@ namespace LengthCalculator1
             //將douCM的數值除以100，也就是從公分轉換成公尺
             //透過string.Format格式化成小數點後共10位的數字，轉型成文字型態，在txtM顯示結果
             txtKM.Text = string.Format("{0:0.##########}", douCM / 100000);
-            txtIn.Text = string.Format("{0:0.##########}", douCM / 2.54); 
+            txtIn.Text = string.Format("{0:0.##########}", douCM / 2.54);
             txtFt.Text = string.Format("{0:0.##########}", douCM / 30.48);
-            txtYard.Text = string.Format("{0:0.##########}", douCM / 91.44);  
-
-
+            txtYard.Text = string.Format("{0:0.##########}", douCM / 91.44);
         }
+        
 
         private void txtM_KeyUp(object sender, KeyEventArgs e)
         {
+            double douM;
+            douM = Convert.ToDouble(txtM.Text);
+            txtCM.Text = string.Format("{0:0.##########}", douM * 100);
+            txtKM.Text = string.Format("{0:0.##########}", douM * 0.001);
+            txtIn.Text = string.Format("{0:0.##########}", douM * 39.3701);
+            txtFt.Text = string.Format("{0:0.##########}", douM * 3.28084);
+            txtYard.Text = string.Format("{0:0.##########}", douM * 1.09361);
 
         }
     }
